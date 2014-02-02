6a8e37a81851644aaf39a1cf96bcd73152a2047d
nathanallen <thenathanator@gmail.com>
2014-02-01 16:09:34 -0800

    initial commit of rails framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FoodunitesUs::Application.config.secret_key_base = 'c0aa7c79c0deb9084ba57d8cdac1076034365eadc5f94398a6d1a21258fdee53a650e0b45d53b8c712bdc629de761836675db22f78270639f7053c3f1c0ced72'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FoodunitesUs::Application.config.session_store :cookie_store, key: '_foodunites_us_session'
