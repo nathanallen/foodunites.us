c1c8877130b6be687f38442a00a4d748de675630
nathanallen <thenathanator@gmail.com>
2014-02-01 18:22:06 -0800

    hard-coded json object for development

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,6 +1,17 @@
 class PhotosController < ApplicationController
 
   def index
+    render json: test_object.to_json
   end
 
+  private
+
+  def test_object
+    names = ["Joey", "Billy"]
+    locations = ["San Francisco, CA", "New York, NY"]
+    tags = ["burger", "hamburger"]
+    links = ["http://thumbs.ifood.tv/files/images/school%20kid%20eating%20fast%20food.jpg","http://www.myhealthnews.co.uk/wp-content/uploads/2012/10/boy-eating-burger.jpg"]
+    
+    {photos:{left:{username:names[0],location:locations[0],tags:tags[0],photo_url:links[0]},right:{username:names[1],location:locations[1],tags:tags[1],photo_url:links[1]}}}
+  end
 end
